6154f87ef1a83085ceec187406cb8f5415a5c962
jskim <mudakim7@gmail.com>
2026-06-21 23:44:25 +0900

    good

---
+++ python_Study/func.py
@@ -0,0 +1,14 @@
+def sort(n1, n2):
+    if n1 < n2:
+        return n1, n2
+    else:
+        return n2, n1
+    
+print(sort(100, 200))
+print(sort(200, 150))
+
+def add(a, b):
+    c = a + b
+    return
+
+print(add(1,2))
