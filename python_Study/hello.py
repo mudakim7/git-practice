a1ffc8574caa556f27952d5e3f6f8afb52f8303d
jskim <mudakim7@gmail.com>
2026-06-21 21:10:45 +0900

    commit

--- python_Study/hello.py
+++ python_Study/hello.py
@@ -1,1 +1,27 @@
+import math
+
 print("hello jskim~~")
+print('jskim' * 20)
+
+x, y = 100, 200
+print(x, y)
+print(x ** 2)
+
+print(type(x))
+
+print(0.1 + 0.1)
+print(0.1 + 0.1 + 0.1)
+
+print(str(100) + 'str')
+
+# name = input('환영합니다.: ')
+# print(name, '님, 어서오세요!!')
+
+if True: 
+   print('a')
+   print('b')
+
+x, y, z = map(int, input('세 정수를 입력하시오: ').split())
+print(x, y, z)
+
+print(math.pi, math.e)
